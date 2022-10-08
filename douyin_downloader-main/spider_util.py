7f14cc8234d7707a59605ecaedce839e7bd37806
liuyichen <1395264153@qq.com>
2022-10-08 19:54:49 +0800

    Add files via upload

--- douyin_downloader-main/spider_util.py
+++ douyin_downloader-main/spider_util.py
@@ -129,6 +129,21 @@ def get_lxml_etree(browser):
 
 def get_comment_info_by_lxml(root, index):
 
+        ####
+    ####
+    ###
+    ##
+    #
+    ####新建一个driver进行子页面抖音号爬取
+    # file_path = os.path.dirname(__file__)
+    # chrome_driver_path = file_path + '/chromedriver.exe'
+    # chrome_options = Options()
+    # chrome_options.add_argument('--disable-gpu')
+    # # chrome_options.add_argument('--headless')
+    # # chrome_options.add_argument('--no-sandbox')
+    # subbrower = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
+    # subbrower.maximize_window()
+    
     # root:      //*[@id="root"]/div/div[2]/div/div/div[1]/div[3]/div/div/div[4]/div[12]
     # time:   	 //*[@id="root"]/div/div[2]/div/div/div[1]/div[3]/div/div/div[4]/div[12]/div/div[2]/div[1]/div[2]/div[1]/p
     # praise: 	 //*[@id="root"]/div/div[2]/div/div/div[1]/div[3]/div/div/div[4]/div[12]/div/div[2]/div[1]/div[3]/div/p/span
@@ -142,6 +157,24 @@ def get_comment_info_by_lxml(root, index):
     main_page_relative_xpath = ".//div[@class='nEg6zlpW']/a/@href"
     praise_relative_xpath = ".//div[@class='rJFDwdFI']/div/p/span"
     comment_time_relative_xpath = "div/div[2]/div[1]/div[2]/div[1]/p"
+    comment_and_img_relative_xpath=".//p[@class='a9uirtCT']/span/span/span/span/span/span/*"
+    user_id = "//span[@class='kbjj_psh']"
+    #评论从1开始标号
+    
+    rise_id = 1
+    
+    comment_hilev_relative_xpath = ".//div[@class='nNNp3deF']/div"
+    comment_hilev_page_relative_xpath = ".//div[@class='nEg6zlpW']/a/@href"
+    comment_hilev_name_relative_xpath = ".//div[@class='nEg6zlpW']/a/span/span/span/span/span/span"
+    comment_hilev_time_relative_xpath = ".//p[@class='dn67MYhq']"
+    #/div[2]/div[1]/div[2]/div[1]/p
+    comment_hilev_praise_relative_xpath = ".//div[@class='rJFDwdFI']/div/p/span"
+    comment_hilev_detail_relative_xpath = ".//p[@class='a9uirtCT']/span/span/span/span/span/span/*"
+    ### 1 find first level comment 
+    ### 2 search high level comment of this comment
+    ### 3 connect with id 
+    ### 4 every comment has a special id
+    ### [comment_meta,[id1,id2,...,idn]=subcomment]
     comment_list = []
     comment_info = {}
 
@@ -169,43 +202,170 @@ def get_comment_info_by_lxml(root, index):
                 main_page=main_page[2:]
             print(f'主页: {main_page}')
             comment_info["main_page"] = main_page
+            #get sub-commment's user_id
+            # subbrower.get("https://" + main_page)
+            # subhtml = get_lxml_etree(subbrower)
+            # idlists = subhtml.xpath(user_id)
+            # if idlists is not None and len(idlists) != 0:
+            #     comment_info["user_id"] = idlists[0].text
+            #     print(idlists[0].text)
+            
 
         comment_time_list = comment_obj.xpath(comment_time_relative_xpath)
         if comment_time_list is not None and len(comment_time_list) != 0:
             comment_time = comment_time_list[0].text
             print(f'评论时间: {comment_time}')
             comment_info["comment_time"] = comment_time
-
+            
+        ####注意
+        ####
+        ####将篇评论文字和图片组装在一起
+        ####https://github.com/liuyichen-doc/douyinCrawler.git
+        comment_info["comment_text"]=""
+        comment_comimg_list = comment_obj.xpath(comment_and_img_relative_xpath)
+        if comment_comimg_list is not None and len(comment_comimg_list) != 0:
+            print("存在评论，文字或者图像")
+            for cur_com in comment_comimg_list:
+                #假设是<text>
+                cur = cur_com.text
+                if cur is None:
+                    cur = '<' + cur_com.xpath("./@src")[0] + '>'
+                comment_info["comment_text"] = comment_info["comment_text"]  + cur
+                
+        #### 注意 本段单独获取了评论的 文本 与 图像
+        ####
+        ####
+        ####
         # 评论内容和图像的获取
         # <=>分隔多条评论
-        comment_info["comment_text"]=""
-        comment_text_list = comment_obj.xpath(comment_texts_relative_xpath)       
-        if comment_text_list is not None and len(comment_text_list) != 0:
-            # comment_text = comment_text_list[0].text
-            # print(f'评论内容: {comment_text}')
-            print("评论字符串 数量为：",len(comment_text_list))
-            for comment_text in comment_text_list:
-                comment_info["comment_text"] = comment_info["comment_text"]+"<=>"+comment_text.text
-        else:
-            print("评论内容为图像或者不可现实的为encoding字符段")
+        # comment_info["comment_text"]=""
+        # comment_text_list = comment_obj.xpath(comment_texts_relative_xpath)       
+        # if comment_text_list is not None and len(comment_text_list) != 0:
+        #     # comment_text = comment_text_list[0].text
+        #     # print(f'评论内容: {comment_text}')
+        #     print("评论字符串 数量为：",len(comment_text_list))
+        #     for comment_text in comment_text_list:
+        #         comment_info["comment_text"] = comment_info["comment_text"]+"<=>"+comment_text.text
+        # else:
+        #     print("评论内容为图像或者不可现实的为encoding字符段")
         
-        #获取评论内容或被多个图像隔开
-        #<span><~><img><~><span>等等
-        comment_img_list = comment_obj.xpath(comment_imgs_relative_xpath)
+        # #获取评论内容或被多个图像隔开
+        # #<span><~><img><~><span>等等
+        # comment_img_list = comment_obj.xpath(comment_imgs_relative_xpath)
 
-        if comment_img_list is not None and len(comment_img_list) != 0:
-            print(f'有评论图像')
-            for comment_img in comment_img_list:
-                print(comment_img.xpath("./@src")[0])
-                # download_img()
-        else:
-            print("该评论图像不存在")
-            
+        # if comment_img_list is not None and len(comment_img_list) != 0:
+        #     print(f'有评论图像')
+        #     for comment_img in comment_img_list:
+        #         print(comment_img.xpath("./@src")[0])
+        #         # download_img()
+        # else:
+        #     print("该评论图像不存在")
+        
         praise_num_list = comment_obj.xpath(praise_relative_xpath)
         if praise_num_list is not None and len(praise_num_list) != 0:
             praise_num = praise_num_list[0].text
             print(f'点赞数: {praise_num}')
             comment_info["praise_num"] = praise_num
+        comment_info["comment_id"] = rise_id
+        ######初始化子评论的id列表
+        comment_info["sub_comments"] = []
+        rise_id = rise_id + 1
+        
+        #########################
+        ###开始多级评论爬虫#######
+        #########################
+        ## 多级评论的获取
+        ## 多级评论位置lists 
+        ## 结构 
+        ## 多级评论共层，指向回复人id或者其他标识
+        ### 
+        ##        获取子评论 div 【pre,[se,tr...]】
+        
+        
+        
+        ####
+        sub_comment_info = {}
+        sub_comments_origin = comment_obj.xpath(comment_hilev_relative_xpath)   
+        
+        
+        for sub_comment in sub_comments_origin:
+            #标记回复对象是否是第一评论
+            flag = 0 
+            
+            #子评论人username
+            sub_name = sub_comment.xpath(comment_hilev_name_relative_xpath)
+            if sub_name is not None and len(sub_name) != 0:
+                user_name = sub_name[0].text
+                print(f'sub用户名: {user_name}')
+                sub_comment_info["user_name"] = user_name
+            else:
+                print("sub评论加载完成")
+                break
+            
+            #子评论人的主页url
+            sub_main_page = sub_comment.xpath(comment_hilev_page_relative_xpath)
+            if sub_main_page is not None and len(sub_main_page) != 0:
+                main_page = sub_main_page[0]
+                if main_page.startswith("//"):
+                    main_page=main_page[2:]
+                print(f'sub主页: {main_page}')
+                sub_comment_info["main_page"] = main_page
+                # subbrower.get(main_page)
+                # subhtml = get_lxml_etree(subbrower)
+                # subidlists = subhtml.xpath(user_id)
+                # if idlists is not None and len(subidlists) != 0:
+                #     sub_comment_info["user_id"] = subidlists[0].text
+                #回复对象不是第一级
+                if len(sub_main_page) == 2:
+                    flag = 1
+                    call_back_page = sub_main_page[1]
+                    
+                    
+            #子评论的时间
+            sub_time = sub_comment.xpath(comment_hilev_time_relative_xpath)
+            if sub_time is not None and len(sub_time) != 0:
+                comment_time = sub_time[0].text
+                print(f'sub评论时间: {comment_time}')
+                sub_comment_info["comment_time"] = comment_time
+
+            
+            #子评论的获赞
+            sub_praise = sub_comment.xpath(comment_hilev_praise_relative_xpath)
+            if sub_praise is not None and len(sub_praise) != 0:
+                praise_num = sub_praise[0].text
+                print(f'sub点赞数: {praise_num}')
+                sub_comment_info["praise_num"] = praise_num
+
+            
+            #子评论的内容
+            sub_comment_info["comment_text"]=""
+            sub_detail = sub_comment.xpath(comment_hilev_detail_relative_xpath)
+            if sub_detail is not None and len(sub_detail) != 0:
+                print("存在评论，文字或者图像")
+                for cur_com in sub_detail:
+                    #假设是<text>
+                    cur = cur_com.text
+                    if cur is None:
+                        cur = '<' + cur_com.xpath("./@src")[0] + '>'
+                    sub_comment_info["comment_text"] = sub_comment_info["comment_text"]  + cur
+                    
+            sub_comment_info["comment_id"] = rise_id
+            sub_comment_info["sub_comments"]=[]
+            if flag == 0:
+                comment_info["sub_comments"].append(rise_id)
+            else:
+                #在所有的comment_list列表中徇众主页为back_page的一项
+                #子评论必定在上级评论之后
+                for index in range(len(comment_list)):
+                    print(comment_list[index]["main_page"])
+                    print(call_back_page)
+                    if comment_list[index]["main_page"] is call_back_page:
+                        comment_list[index]["sub_comments"].append(rise_id)
+                        break
+            #保存多级评论
+            comment_list.append(sub_comment_info)
+            rise_id = rise_id + 1
+        #将第一级评论保存
         comment_list.append(comment_info)
     return comment_list
 
